d29bf7d28cc79e5db32352679483dd28d22c8d48
HarryAnderso <harrystuartanderson@gmail.com>
2026-03-28 15:00:17 -0400

    first snapshot for the first portion of the final assingment

--- Assets/Scripts/Tasks/GuardChaseAT.cs
+++ Assets/Scripts/Tasks/GuardChaseAT.cs
@@ -1,28 +1,42 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
+using UnityEngine.AI;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class GuardChaseAT : ActionTask {
+		NavMeshAgent Navi;
+		public GameObject player;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
-			return null;
+
+			Navi = agent.GetComponent<NavMeshAgent>();
+
+            return null;
 		}
 
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+			
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
-		}
+			if (player == null || Vector3.Distance(player.transform.position, agent.transform.position) >10)
+			{
+				EndAction(false);
+            }
+            else
+            {
+				Navi.SetDestination(player.transform.position);
+            }
+        }
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
